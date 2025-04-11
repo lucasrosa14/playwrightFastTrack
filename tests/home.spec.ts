8c25be96d3904d2b6d73efeae898ccbdf559f084
Lucas Rosa <contact.lucassr@gmail.com>
2025-04-11 14:39:00 -0300

    Ajuste da url e assert do teste home.spec.ts

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,7 +1,7 @@
 import {test, expect} from '@playwright/test'
 
 test('aplicação deve estar no ar', async ({page}) => {
-    await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/login')
-    await expect(page).toHaveTitle('Account Login')
+    await page.goto('https://ecommerce-playground.lambdatest.io')
+    await expect(page).toHaveTitle('Your Store')
     
 })
